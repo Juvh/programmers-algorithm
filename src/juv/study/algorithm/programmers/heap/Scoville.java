ed0e4b2579fa4308fe65c4bd81d58aa8d96211a0
Juv <dev.juv@gmail.com>
2022-05-17 01:11:23 +0900

    heap - Scoville

---
+++ src/juv/study/algorithm/programmers/heap/Scoville.java
@@ -0,0 +1,43 @@
+package juv.study.algorithm.programmers.heap;
+
+import java.util.*;
+
+public class Scoville {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.solution(new int[]{1, 2, 3, 9, 10, 12}, 7));
+    }
+
+    static class Solution {
+        public int solution(int[] scoville, int K) {
+
+            PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+            for (int s : scoville) {
+                priorityQueue.add(s);
+            }
+
+            int cnt = 0;
+            while (true) {
+                Integer first = priorityQueue.poll();
+                if (first > K) {
+                    return cnt;
+                }
+                if (priorityQueue.size() == 1) {
+                    return -1;
+                }
+                Integer second = priorityQueue.poll();
+
+                int calculated = calculate(first, second);
+                priorityQueue.add(calculated);
+                cnt++;
+            }
+        }
+
+        private int calculate(int a, int b) {
+            if (a > b) {
+                return b + (a * 2);
+            }
+            return a + (b * 2);
+        }
+    }
+}
